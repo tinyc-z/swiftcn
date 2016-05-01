07fb4bda4af21f1ee8df8c991dc0638d027d3a76
ibcker <happymiyu@gmail.com>
2016-05-02 00:07:36 +0800

    过滤器增加到节点中

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,13 +2,13 @@ class NodesController < ApplicationController
   
   def show
     @node = Node.find params_id
-    @topics = @node.topics.includes(:user,:node,:last_reply_user).order('updated_at DESC').paginate(params_page)
+    @topics = @node.topics.includes(:user,:node,:last_reply_user).filter(params[:filter]).paginate(params_page)
   end
 
   def jobs
     @node = Node.new
     @node.name = 'site.Jobs'.lang
-    @topics = Topic.where(node_id:Settings.JOB_NODES).includes(:user,:node,:last_reply_user).order('updated_at DESC').paginate(params_page)
+    @topics = Topic.where(node_id:Settings.JOB_NODES).includes(:user,:node,:last_reply_user).filter(params[:filter]).paginate(params_page)
     render 'show'
   end
 
