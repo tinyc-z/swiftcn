4e0639cd1943e10c1974dd689464679ec0fd8766
ibcker <happymiyu@gmail.com>
2016-05-01 16:50:27 +0800

    fix两个N+1问题

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,13 +2,13 @@ class NodesController < ApplicationController
   
   def show
     @node = Node.find params_id
-    @topics = @node.topics.order('updated_at DESC').paginate(:page => params[:page])
+    @topics = @node.topics.includes(:user,:node,:last_reply_user).order('updated_at DESC').paginate(:page => params[:page])
   end
 
   def jobs
     @node = Node.new
     @node.name = t('site.Jobs',default:'site.Jobs'.split('.').last)
-    @topics = Topic.where(node_id:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,25,26,27]).order('updated_at DESC').paginate(:page => params[:page])
+    @topics = Topic.where(node_id:[2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,25,26,27]).includes(:user,:node,:last_reply_user).order('updated_at DESC').paginate(:page => params[:page])
     render 'show'
   end
 
