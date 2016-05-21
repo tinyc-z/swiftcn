f0ef7780abc31ebc7be05c40cfa4a2499a73cf7c
ibcker <happymiyu@gmail.com>
2016-05-22 07:10:14 +0800

    update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519191429) do
+ActiveRecord::Schema.define(version: 20160521225409) do
 
   create_table "appends", force: :cascade do |t|
     t.integer  "topic_id",   limit: 4
