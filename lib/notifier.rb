52031ae4e96c714447e6cb996c84512fdfaceb68
ibcker <happymiyu@gmail.com>
2016-06-07 03:36:16 +0800

    fix 发管理员贴失败bug

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -8,8 +8,8 @@ class Notifier
   def self.send_admin_notifi(url)
     admins = User.where(role:Role.admin).pluck(:email,:name)
     if admins.present?
-      vars = {to:admins.map{|u|u[0]},sub:{'%name%' => admins.map{|u|u[1]}, '%url%'=>[url]}}.to_json
-      Sendcloud::Mail.send_template(template_invoke_name:'admin_notification', subject: '有新贴 - swift中国', from: 'siri@mail.swiftcn.io', fromname: 'Siri', substitution_vars: vars)  
+      vars = {to:admins.map{|u|u[0]},sub:{'%name%' => admins.map{|u|u[1]}, '%url%'=>admins.map{ url } }}.to_json
+      Sendcloud::Mail.send_template(template_invoke_name:'admin_notification', subject: '有新贴 - swift中国', from: 'siri@mail.swiftcn.io', fromname: 'Siri', substitution_vars: vars)
     end
   end
 
